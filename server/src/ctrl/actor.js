19a47b41cbc9614d1483e0062f30007ed2fa1841
TheSimorgh <TheSimurgh30@proton.me>
2023-08-05 06:03:13 +0500

    maintaing actor routing

---
+++ server/src/ctrl/actor.js
@@ -0,0 +1,48 @@
+exports.all_actor = async (req, res) => {
+  res.json({
+    message: 1,
+  });
+};
+exports.single_actor = async (req, res) => {
+  res.json({
+    message: 1,
+  });
+};
+
+exports.create_actor = async (req, res) => {
+    res.json({
+      message: 1,
+    });
+  };
+
+
+
+
+exports.update_actor = async (req, res) => {
+  res.json({
+    message: 1,
+  });
+};
+exports.delete_actor = async (req, res) => {
+  res.json({
+    message: 1,
+  });
+};
+
+exports.search = async (req, res) => {
+    res.json({
+      message: 1,
+    });
+  };
+  
+  exports.latest_uploads = async (req, res) => {
+    res.json({
+      message: 1,
+    });
+  };
+  
+exports.test = async (req, res) => {
+  res.json({
+    message: 1,
+  });
+};
--- server/src/routes/actor.js
+++ server/src/routes/actor.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const { all_actor, single_actor, create_actor, update_actor, delete_actor, search, latest_uploads } = require("../ctrl/actor");
 const router = express.Router();
 router.get("/",(req,res)=>{console.log("1")} );
 
 
+
+router.get("/all",all_actor)
+router.get("/single/:id",single_actor)
+router.post("/create",create_actor)
+router.put("/update/:actorId",update_actor)
+router.delete("/:actorId",delete_actor)
+router.get("/search",search)
+router.get("/latest-uploads",latest_uploads)
 module.exports = router;
